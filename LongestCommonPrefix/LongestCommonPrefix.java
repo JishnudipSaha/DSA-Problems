e4872f1395c17ea417e3e4139bbac202fa6b62d2
JishnudipSaha <jishnudipsaha.10a.15.02062004@gmail.com>
2026-01-18 11:48:38 +0530

    Longest Common Prefix DSA from leetcode

---
+++ LongestCommonPrefix/LongestCommonPrefix.java
@@ -0,0 +1,29 @@
+package LongestCommonPrefix;
+
+public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+            
+
+        String prefix = strs[0];  // "flower" at index 0
+
+        for(int i = 1; i < strs.length; i++){
+            while (!strs[i].startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length()-1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+    public static void main(String[] args) {
+        String[] strs = {"flower", "flow", "flight"};
+
+        // Solution sol = new Solution();
+        System.out.println("LCP: " + longestCommonPrefix(strs));
+    }
+}
