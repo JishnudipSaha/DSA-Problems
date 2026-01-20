821b0a4507668be7216911cf9e31d97c775098d1
JishnudipSaha <jishnudipsaha.10a.15.02062004@gmail.com>
2026-01-20 11:59:45 +0530

    Power calculation of core concept understood and implemented

--- MathsPower/MathsPower.java
+++ MathsPower/MathsPower.java
@@ -9,32 +9,20 @@ public class MathsPower {
         x.(((x)^2)^(n-1 / 2))
          */
     public static double myPow(double x, int n) {
-        if (n == 0) {
-            return 1.0;
+        long N = n;   // prevent overflow
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
         }
-        int N = n;
         double ans = 1.0;
-        n = Math.abs(n);
-        // checking even or odd then procedding as per to it.
-        if (n%2 == 0) {
-            //if even: ((x)^2)^n/2
-            int num = n/2;
-            double a = (x*x); // 4
-            while (num != 0) { // 
-                ans = ans * a;
-                num -= 1;
+        while (N > 0) {
+            if (N % 2 == 1) {
+                ans *= x;
             }
-        }else{
-            //if odd: x.(((x)^2)^(n-1 / 2))
-            int num = (n-1)/2;
-            double a = (x*x);
-            while (num != 0) {
-                ans = ans * a;
-                num -= 1;
-            }
-            ans = x * ans;
+            x *= x;
+            N /= 2;
         }
-        return (N>=0)? ans : (1/ans);
+        return ans;
     }
     public static void main(String[] args) {
         System.out.println(myPow(2, -10));
