b39dc9eb00b9140b5159795ae46d81a61a538b4e
JishnudipSaha <jishnudipsaha.10a.15.02062004@gmail.com>
2026-01-20 11:45:33 +0530

    Understood the concept but not optimized

--- MathsPower/MathsPower.java
+++ MathsPower/MathsPower.java
@@ -1,19 +1,42 @@
 package MathsPower;
 
 public class MathsPower {
+    /*
+        need proper maths formulation:
+        for n if even :
+        ((x)^2)^n/2
+        if n  is odd:
+        x.(((x)^2)^(n-1 / 2))
+         */
     public static double myPow(double x, int n) {
-        if (n == 0)  return 1.0;
-        
-        int a = n;
+        if (n == 0) {
+            return 1.0;
+        }
+        int N = n;
         double ans = 1.0;
         n = Math.abs(n);
-        while (n != 0) {
-            ans = ans * x;
-            n--; // (2,4) 4->2 3->4 2->8 1->16
+        // checking even or odd then procedding as per to it.
+        if (n%2 == 0) {
+            //if even: ((x)^2)^n/2
+            int num = n/2;
+            double a = (x*x); // 4
+            while (num != 0) { // 
+                ans = ans * a;
+                num -= 1;
+            }
+        }else{
+            //if odd: x.(((x)^2)^(n-1 / 2))
+            int num = (n-1)/2;
+            double a = (x*x);
+            while (num != 0) {
+                ans = ans * a;
+                num -= 1;
+            }
+            ans = x * ans;
         }
-        return (a>=0)? ans : (1/ans);
+        return (N>=0)? ans : (1/ans);
     }
     public static void main(String[] args) {
-        System.out.println(myPow(2, 4));
+        System.out.println(myPow(2, -10));
     }
 }
