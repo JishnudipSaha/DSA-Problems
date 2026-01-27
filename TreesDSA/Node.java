5ffa9ca9992b7c21b5fb784cf1ecb1ddbc331aad
JishnudipSaha <jishnudipsaha.10a.15.02062004@gmail.com>
2026-01-28 01:21:56 +0530

    Added TreeDSA folder for tree related content

---
+++ TreesDSA/Node.java
@@ -0,0 +1,16 @@
+package TreesDSA;
+
+public class Node {
+    int val;
+    Node nextNode = null;
+    Node prevNode = null;
+    Node(){}
+    Node(int val){
+        this.val = val;
+    }
+    Node(int val, Node nextNode, Node prevNode){
+        this.val = val;
+        this.nextNode = nextNode;
+        this.prevNode = prevNode;
+    }
+}
---
+++ TreesDSA/TreeTraversal.java
@@ -0,0 +1,26 @@
+package TreesDSA;
+
+public class TreeTraversal {
+    Node head = null;
+
+    Node createNode(int val){
+        return new Node(val);
+    }
+
+    Node addNode(int val){
+        if (head == null) {
+            head = createNode(val);
+        }else{
+            Node tempNode = head;
+            while (tempNode.nextNode != null) {
+                tempNode = tempNode.nextNode;
+            }
+            tempNode.nextNode = createNode(val);
+        }
+        return new Node(val);
+    }
+    public static void main(String[] args) {
+        TreeTraversal traversal = new TreeTraversal();
+        traversal.addNode(25);
+    }
+}
