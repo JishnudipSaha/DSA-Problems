740b9b68b59ee2c934d24ae517531f1ba1cb9dca
JishnudipSaha <jishnudipsaha.10a.15.02062004@gmail.com>
2026-02-10 12:46:42 +0530

    Added to arrays FirstOccur and CountSay

---
+++ Arrays/CountSay.java
@@ -0,0 +1,24 @@
+public class CountSay {
+    public String countAndSay(int n) {
+        if (n == 1) return "1";
+
+        String prev = countAndSay(n-1); // previous string
+        StringBuilder ans = new StringBuilder();
+
+        int count = 1;
+        for (int i = 1; i < prev.length(); i++){
+            if (prev.charAt(i) == prev.charAt(i-1)) count ++;
+            else {
+                ans.append(count);
+                ans.append(prev.charAt(i-1));
+                count = 1;
+            }
+        }
+        ans.append(count);
+        ans.append(prev.charAt(prev.length() -1));
+        return ans.toString();
+    }
+    public static void main(String[] args) {
+
+    }
+}
