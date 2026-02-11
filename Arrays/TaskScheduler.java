1c7e362bb5456a7f35f82487095e9f497cbdbe47
JishnudipSaha <jishnudipsaha.10a.15.02062004@gmail.com>
2026-02-11 10:14:53 +0530

    Added Task Scheduler in Arrays

---
+++ Arrays/TaskScheduler.java
@@ -0,0 +1,28 @@
+public class TaskScheduler {
+    public int leastInterval(char[] tasks, int n) {
+        int[] freq = new int[26];
+        for (char c: tasks){
+            freq[c - 'A']++;
+        }
+
+        int maxFreq = 0;
+        for (int f: freq){
+            maxFreq = Math.max(maxFreq, f);
+        }
+
+        int maxCount = 0;
+        for (int i: freq){
+            if (i == maxFreq) maxCount++;
+        }
+
+        int partCount = maxFreq - 1;
+        int partLength = n + 1;
+        int minTime = partCount * partLength + maxCount;
+
+        return Math.max(tasks.length, minTime);
+    }
+    public static void main(String[] args) {
+        TaskScheduler scheduler = new TaskScheduler();
+        System.out.println(scheduler.leastInterval(new char[]{'A', 'A', 'A', 'B', 'B', 'B'}, 2));;
+    }
+}
