d43ab3477f8f020735f192e0cc345dd51a4a6101
JishnudipSaha <jishnudipsaha.10a.15.02062004@gmail.com>
2026-02-11 20:30:52 +0530

    Valid Sudoku DSA problem added

---
+++ Arrays/ValidSudoku.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+
+public class ValidSudoku {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> seen = new HashSet<>();
+
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                char num = board[r][c];
+
+                if (num == '.') continue;
+
+                String rowKey = num + " in row " + r;
+                String colKey = num + " in col " + c;
+                String boxKey = num + " in box " + (r / 3) + "-" + (c / 3);
+
+                if (!seen.add(rowKey) || !seen.add(colKey) || !seen.add(boxKey)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
