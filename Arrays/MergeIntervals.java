9e79fc83951d768bc3919391715ffaa930f797f2
JishnudipSaha <jishnudipsaha.10a.15.02062004@gmail.com>
2026-02-15 22:54:32 +0530

    Added Merged Intervals DSA problems

---
+++ Arrays/MergeIntervals.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeIntervals {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a,b)->Integer.compare(a[0], b[0]));
+        List<int[]> merged = new ArrayList<>();
+        for (int[] interval: intervals){
+            if (merged.isEmpty() || interval[0]>merged.get(merged.size() - 1)[1]){
+                merged.add(interval);
+            }else {
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], interval[1]);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+
+    public static void main(String[] args) {
+    }
+}
